307cd7bf5b1675f8fbc616f42d196d478b4af7c1
nvvetal <vitaliy.grinchishin@gmail.com>
2014-01-30 18:28:03 +0200

    shop send gift fix

--- lib/migrate_users.php
+++ lib/migrate_users.php
@@ -6,7 +6,7 @@ $q = "
     FROM phpbb_users as u
     LEFT JOIN phpbb_profile_fields_data AS pf ON (u.user_id = pf.user_id)
     ORDER BY u.user_id ASC
-    LIMIT 100    
+    LIMIT 100
 ";
 
 $data = SQLGetRows($q, $DBFactory->get_db_handle('forum'));
@@ -15,18 +15,18 @@ $Images = Registry::get('Images');
 $User = new User($DBFactory->get_db_handle('rakscom'));
 
 foreach ($data as $userData){
-	$file = '../modules/forum/images/avatars/upload/3953387675efec4c0bd9ae15685ad5ca_'.$userData['user_avatar'];
-	$needConvert = true;
-	if(!file_exitsts($file)){
-		$file = '../modules/forum/images/avatars/upload/c9589b706a984dcdeb11717419bb6a50_'.$userData['user_avatar'];
-		if(file_exists($file)) $needConvert = false;
-	}
-	$imageId = 0;
-	if($needConvert){
-   		$res = $Images->copy_image($file, $GLOBALS['IMAGE_UPLOAD_ORIGINAL_PATH']);
-    	$imageId = ($res['res'] != true) ? $res['ID'] : 0; 	
-	}
-	//images/avatars/upload
+    $file = '../modules/forum/images/avatars/upload/3953387675efec4c0bd9ae15685ad5ca_'.$userData['user_avatar'];
+    $needConvert = true;
+    if(!file_exitsts($file)){
+        $file = '../modules/forum/images/avatars/upload/c9589b706a984dcdeb11717419bb6a50_'.$userData['user_avatar'];
+        if(file_exists($file)) $needConvert = false;
+    }
+    $imageId = 0;
+    if($needConvert){
+        $res = $Images->copy_image($file, $GLOBALS['IMAGE_UPLOAD_ORIGINAL_PATH']);
+        $imageId = ($res['res'] != true) ? $res['ID'] : 0;
+    }
+    //images/avatars/upload
     $params = array(
         'forum'         => $userData['user_id'],
         'image_id'      => $imageId,
@@ -38,22 +38,22 @@ foreach ($data as $userData){
         'p_icq'         => $userData['user_icq'],
         'p_url'         => str_replace('http://', '' ,$userData['user_website']),
         'p_profession'  => $userData['user_occ'],
-    );	
-	
-    
-	$uParams = array(
-		'login' 	=> $userData['username'],
-		'password'	=> $userData['user_password'],
-		'email'		=> $userData['user_email'],
-		'state'		=> 'active',
-	);	
-	$userId = $User->register_user($uParams);
-	
-	foreach ($params as $pk=>$pval){
-		$User->set_user_value($userId, $pk, $pval);
-	}
-	
-	if($imageId > 0)$Images->assign_image($imageId, $userId, 'user');
+    );
+
+
+    $uParams = array(
+        'login' 	=> $userData['username'],
+        'password'	=> $userData['user_password'],
+        'email'		=> $userData['user_email'],
+        'state'		=> 'active',
+    );
+    $userId = $User->register_user($uParams);
+
+    foreach ($params as $pk=>$pval){
+        $User->set_user_value($userId, $pk, $pval);
+    }
+
+    if($imageId > 0)$Images->assign_image($imageId, $userId, 'user');
 }
 /*
 p_hobby
@@ -62,4 +62,3 @@ p_sex
 p_skype
 p_url
 */
-?>
