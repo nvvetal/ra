bed0ee4fd73faab726a0496dc1241e44cbb8d26c
nvvetal <vitaliy.grinchishin@gmail.com>
2014-02-04 17:29:45 +0200

    bbcode url

--- modules/calendar/calendar_forum_bitfield.class.php
+++ modules/calendar/calendar_forum_bitfield.class.php
@@ -1,93 +1,93 @@
 <?php
 class calendar_forum_bitfield
 {
-	var $data;
+    var $data;
 
-	function bitfield($bitfield = '')
-	{
-		$this->data = base64_decode($bitfield);
-	}
+    function bitfield($bitfield = '')
+    {
+        $this->data = base64_decode($bitfield);
+    }
 
-	/**
-	*/
-	function get($n)
-	{
-		// Get the ($n / 8)th char
-		$byte = $n >> 3;
+    /**
+     */
+    function get($n)
+    {
+        // Get the ($n / 8)th char
+        $byte = $n >> 3;
 
-		if (strlen($this->data) >= $byte + 1)
-		{
-			$c = $this->data[$byte];
-	
-			// Lookup the ($n % 8)th bit of the byte
-			$bit = 7 - ($n & 7);
-			return (bool) (ord($c) & (1 << $bit));
-		}
-		else
-		{
-			return false;
-		}
-	}
+        if (strlen($this->data) >= $byte + 1)
+        {
+            $c = $this->data[$byte];
 
-	function set($n)
-	{
-		$byte = $n >> 3;
-		$bit = 7 - ($n & 7);
+            // Lookup the ($n % 8)th bit of the byte
+            $bit = 7 - ($n & 7);
+            return (bool) (ord($c) & (1 << $bit));
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-		if (strlen($this->data) >= $byte + 1)
-		{
-			$this->data[$byte] = $this->data[$byte] | chr(1 << $bit);
-		}
-		else
-		{
-			$this->data .= str_repeat("\0", $byte - strlen($this->data));
-			$this->data .= chr(1 << $bit);
-		}
-	}
+    function set($n)
+    {
+        $byte = $n >> 3;
+        $bit = 7 - ($n & 7);
 
-	function clear($n)
-	{
-		$byte = $n >> 3;
+        if (strlen($this->data) >= $byte + 1)
+        {
+            $this->data[$byte] = $this->data[$byte] | chr(1 << $bit);
+        }
+        else
+        {
+            $this->data .= str_repeat("\0", $byte - strlen($this->data));
+            $this->data .= chr(1 << $bit);
+        }
+    }
 
-		if (strlen($this->data) >= $byte + 1)
-		{
-			$bit = 7 - ($n & 7);
-			$this->data[$byte] = $this->data[$byte] &~ chr(1 << $bit);
-		}
-	}
+    function clear($n)
+    {
+        $byte = $n >> 3;
 
-	function get_blob()
-	{
-		return $this->data;
-	}
+        if (strlen($this->data) >= $byte + 1)
+        {
+            $bit = 7 - ($n & 7);
+            $this->data[$byte] = $this->data[$byte] &~ chr(1 << $bit);
+        }
+    }
 
-	function get_base64()
-	{
-		return base64_encode($this->data);
-	}
+    function get_blob()
+    {
+        return $this->data;
+    }
 
-	function get_bin()
-	{
-		$bin = '';
-		$len = strlen($this->data);
+    function get_base64()
+    {
+        return base64_encode($this->data);
+    }
 
-		for ($i = 0; $i < $len; ++$i)
-		{
-			$bin .= str_pad(decbin(ord($this->data[$i])), 8, '0', STR_PAD_LEFT);
-		}
+    function get_bin()
+    {
+        $bin = '';
+        $len = strlen($this->data);
 
-		return $bin;
-	}
+        for ($i = 0; $i < $len; ++$i)
+        {
+            $bin .= str_pad(decbin(ord($this->data[$i])), 8, '0', STR_PAD_LEFT);
+        }
 
-	function get_all_set()
-	{
-		return array_keys(array_filter(str_split($this->get_bin())));
-	}
+        return $bin;
+    }
 
-	function merge($bitfield)
-	{
-		$this->data = $this->data | $bitfield->get_blob();
-	}
+    function get_all_set()
+    {
+        return array_keys(array_filter(str_split($this->get_bin())));
+    }
+
+    function merge($bitfield)
+    {
+        $this->data = $this->data | $bitfield->get_blob();
+    }
 }
 
 ?>
