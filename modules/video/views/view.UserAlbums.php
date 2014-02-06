eefdf1bf2e678cf8281f9f3c7a25e9418934c018
nvvetal <vitaliy.grinchishin@gmail.com>
2014-02-06 09:51:10 +0200

    video fix

--- modules/video/views/view.UserAlbums.php
+++ modules/video/views/view.UserAlbums.php
@@ -4,13 +4,9 @@ function videoViewUserAlbums(View $View){
     $page                       = isset($_REQUEST['page']) ? intval($_REQUEST['page']) : 1;
     $perPage                    = 10;
     $DBFactory                  = Registry::get('DBFactory');
-    $Session                    = Registry::get('Session');
-    $sessionId                  = Registry::get('s');
-    $userId                     = $Session->get_value($sessionId, 'user_id');
+    $userId                     = isset($_REQUEST['user_id']) ? intval($_REQUEST['user_id']) : 0;
     $Albums                     = new VideoAlbums($DBFactory->get_db_handle('rakscom'));
     $userAlbums                 = $Albums->byOwner('user', $userId, 'DESC', $page, 10);
     $returnParams['userAlbums'] = $userAlbums;      
     return $returnParams;
 }
-
-?>
