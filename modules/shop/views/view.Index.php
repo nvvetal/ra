60a69f120faa30957d0bd503b31177b523b69d08
nvvetal <vitaliy.grinchishin@gmail.com>
2014-02-13 13:55:22 +0200

    gift for exact user

--- modules/shop/views/view.Index.php
+++ modules/shop/views/view.Index.php
@@ -3,6 +3,22 @@ function shopViewIndex(View $View){
     $returnParams                   = array();
     $DBFactory                      = Registry::get('DBFactory');
     $Shops 	                        = new Shops($DBFactory->get_db_handle('rakscom'));
+    $userForumId = isset($_REQUEST['user_forum_id']) ? $_REQUEST['user_forum_id'] : '';
+    $userId = isset($_REQUEST['user_id']) ? $_REQUEST['user_id'] : '';
+    $user = Registry::get('User');
+    /**
+     * @var $session Session
+     */
+    $session = Registry::get('Session');
+    $sessId = Registry::get('s');
+    if(empty($userId) && !empty($userForumId)){
+        $foundUserId = $user->findUserIdByForumId($userForumId);
+        if($foundUserId !== false) $userId = $foundUserId;
+    }
+    if(!empty($userId) && $user->is_user_id_exists($userId))
+    {
+        $session->set_value($sessId, 'shop_user_id', $userId);
+    }
     $shopsData                     	= $Shops->all();
     $returnParams['shopsData']     	= $shopsData;
     return $returnParams;
--- modules/shop/views/view.Shop.php
+++ modules/shop/views/view.Shop.php
@@ -1,22 +1,7 @@
 <?php
 function shopViewShop(View $View){
     $shopId = isset($_REQUEST['shop_id']) ? $_REQUEST['shop_id'] : 0;
-    $userForumId = isset($_REQUEST['user_forum_id']) ? $_REQUEST['user_forum_id'] : '';
-    $userId = isset($_REQUEST['user_id']) ? $_REQUEST['user_id'] : '';
-    $user = Registry::get('User');
-    /**
-     * @var $session Session
-     */
-    $session = Registry::get('Session');
-    $sessId = Registry::get('s');
-    if(empty($userId) && !empty($userForumId)){
-        $foundUserId = $user->findUserIdByForumId($userForumId);
-        if($foundUserId !== false) $userId = $foundUserId;
-    }
-    if(!empty($userId) && $user->is_user_id_exists($userId))
-    {
-        $session->set_value($sessId, 'shop_user_id', $userId);
-    }
+
     $returnParams = array();
     $DBFactory = Registry::get('DBFactory');
     try {
