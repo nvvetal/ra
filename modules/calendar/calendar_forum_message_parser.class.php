ad6538c952a4c08e7975f2e4e658bad000f173e8
nvvetal <vitaliy.grinchishin@gmail.com>
2014-12-12 15:53:47 +0200

    logo

--- modules/calendar/calendar_forum_message_parser.class.php
+++ modules/calendar/calendar_forum_message_parser.class.php
@@ -126,8 +126,8 @@ class calendar_forum_message_parser
         $data = preg_replace('/\[url\]([^\[]+)\[\/url\]/ims', '<!-- m --><a class="postlink" href="$1">$1</a><!-- m -->', $data);
         $data = preg_replace('/\[url\=([^\[]+)\]([^\[]+)\[\/url\]/ims', '<!-- m --><a class="postlink" href="$1">$2</a><!-- m -->', $data);
 
-        $pattern = '(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()\<>]+|\(([^\s()<>]+|(\([^\s()\<>]+\)))*\))+(?:\(([^\s()\<>]+|(\([^\s()\<>]+\)))*\)|[^\s`!()\[\]{};:\'".,\<>?«»“”‘’]))';
-        $data = preg_replace_callback("#$pattern#i", function($matches) {
+        $pattern = '(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
+        $data = preg_replace_callback("/$pattern/ims", function($matches) {
             $input = $matches[1];
             $url = preg_match('!^https?://!i', $input) ? $input : "http://$input";
             return '<!-- m --><a class="postlink" href="' . $url . '">'.$url.'</a><!-- m -->';
