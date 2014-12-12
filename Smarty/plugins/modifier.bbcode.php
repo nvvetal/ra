6e8220364a337f1490ac54c29c0ded4b70d1253e
nvvetal <vitaliy.grinchishin@gmail.com>
2014-12-12 16:08:49 +0200

    logo

--- Smarty/plugins/modifier.bbcode.php
+++ Smarty/plugins/modifier.bbcode.php
@@ -22,7 +22,7 @@ function smarty_modifier_bbcode($string)
     $string = preg_replace("/\[\/s\]/i","</s>", $string);
 
 	$string = preg_replace("/\[url=([^\]]+)\](.*?)\[\/url\]/i","$1", $string);
-	$string = preg_replace("/\[url\](.*?)\[\/url\]/i","$1", $string);
+	$string = preg_replace("/\[url\]([^\[]+)\[\/url\]/i","$1", $string);
 	$string = preg_replace("/\[size=([^\]]+)\](.*?)\[\/size\]/i","<font size=\"$1\">$2</font>", $string);
 
 	$string = preg_replace("/\[url\](.*?)\[\/url\]/i","<a href=\"$1\">$1</a>", $string);
@@ -31,7 +31,7 @@ function smarty_modifier_bbcode($string)
 	$string = preg_replace("/\[quote.*?\](.*?)\[\/quote\]/i","<span class=\"quoteStyle\">$1</span>&nbsp;", $string);
     #http://daringfireball.net/2010/07/improved_regex_for_matching_urls
     $pattern = '(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
-    $data = preg_replace_callback("#$pattern#i", function($matches) {
+    $string = preg_replace_callback("#$pattern#i", function($matches) {
         $input = $matches[1];
         $url = preg_match('!^https?://!i', $input) ? $input : "http://$input";
         return '<a href="' . $url . '">'.$url.'</a>';
@@ -39,5 +39,3 @@ function smarty_modifier_bbcode($string)
 
 	return $string;
 }
-
-?>
