cedaeebc6a1c900013cd4884636afe21b5d16ed5
nvvetal <vitaliy.grinchishin@gmail.com>
2015-10-10 16:10:38 +0300

    mailer

--- internal/advertise.php
+++ internal/advertise.php
@@ -2,7 +2,7 @@
 //exit;
 //error_reporting(E_ALL);
 ini_set('memory_limit', '200M');
-$advertise_company_id = 'adv307';
+$advertise_company_id = 'adv308';
 require_once('verifyEmail.php');
 require_once('../lib/config.php');
 require_once($GLOBALS['CLASSES_DIR']."DBFactory.class.php");
