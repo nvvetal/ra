21bfcede397ff7e8b5f4931a4d27477a6f840395
nvvetal <vitaliy.grinchishin@gmail.com>
2017-10-28 10:24:01 +0300

    banner

--- internal/advertise.php
+++ internal/advertise.php
@@ -4,7 +4,7 @@
 ini_set('memory_limit', '500M');
 //$advertise_company_id = 'adv541';
 
-$advertise_company_id = 'adv591';
+$advertise_company_id = 'adv592';
 require_once('verifyEmail.php');
 require_once('../lib/config.php');
 require_once($GLOBALS['CLASSES_DIR']."DBFactory.class.php");
